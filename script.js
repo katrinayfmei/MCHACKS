3323d12c630607e4e21092087977fd09616f30c8
Katrina <88742230+katrinayfmei@users.noreply.github.com>
2023-01-29 08:34:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
+$('.Count').each(function () {
+    var $this = $(this);
+    jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+      duration: 1000,
+      easing: 'swing',
+      step: function () {
+        $this.text(Math.ceil(this.Counter));
+      }
     });
-});
+  });
